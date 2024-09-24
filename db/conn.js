a66c53aad495d28b138d0549fa72a75f41478fd5
MarcioAlex-x <marcioalex586@gmail.com>
2024-09-24 10:01:45 -0300

    upload do DB

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 const conn = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user:'root',
-    password: 'root',
-    database: 'pjsenai'
+    host: 'sql111.infinityfree.com',
+    user:'if0_37375923',
+    password: '1g7kXDMoZwHbB',
+    database: 'if0_37375923_pjsenai	'
 })
 
 module.exports = conn
