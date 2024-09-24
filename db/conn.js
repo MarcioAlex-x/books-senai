ef184c3728fae381588e55cc27213becc406c81c
MarcioAlex-x <marcioalex586@gmail.com>
2024-09-24 10:27:47 -0300

    change start

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,8 @@ const conn = mysql.createPool({
     host: 'sql111.infinityfree.com',
     user:'if0_37375923',
     password: '1g7kXDMoZwHbB',
-    database: 'if0_37375923_pjsenai	'
+    database: 'if0_37375923_pjsenai	',
+    port: 3306
 })
 
 module.exports = conn
